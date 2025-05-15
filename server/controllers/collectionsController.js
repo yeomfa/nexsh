45d327690a035a30be1f2379249f872abd01554e
yeomfa <yeomfa@gmail.com>
2025-05-14 21:26:41 -0500

    Get collection and checkId middleware ready

--- server/controllers/collectionsController.js
+++ server/controllers/collectionsController.js
@@ -5,6 +5,20 @@ const collectionsArr = JSON.parse(
   readFileSync(`${dirname}/../resources/collections.json`),
 );
 
+const checkId = (req, res, next, val) => {
+  const collection = collectionsArr.find((col) => col.id === +val);
+
+  if (!collection) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  next();
+};
+
+// Get all
 const getAllCollections = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -14,11 +28,27 @@ const getAllCollections = (req, res) => {
     },
   });
 };
-const getCollection = (req, res) => {};
-const createCollection = (req, res) => {};
-const updateCollection = (req, res) => {};
-const deleteCollection = (req, res) => {};
+
+// Get collection
+const getCollection = (req, res) => {
+  const { id } = req.params;
+  const collection = collectionsArr.find((col) => col.id === +id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      collection,
+    },
+  });
+};
+
+// Create
+const createCollection = (req, res) => { };
+const updateCollection = (req, res) => { };
+const deleteCollection = (req, res) => { };
 
 export default {
   getAllCollections,
+  getCollection,
+  checkId,
 };
--- server/routes/collectionsRoutes.js
+++ server/routes/collectionsRoutes.js
@@ -3,4 +3,7 @@ import controller from '../controllers/collectionsController.js';
 
 export const collectionsRouter = Router();
 
+collectionsRouter.param('id', controller.checkId);
+
 collectionsRouter.route('/').get(controller.getAllCollections);
+collectionsRouter.route('/:id').get(controller.getCollection);
